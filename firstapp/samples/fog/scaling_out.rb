25d54b1d4506177eb9b6ab7f9daad5d899dff4dd
Zhang Xiaowei <zero00072@gmail.com>
2016-02-19 01:21:03 +0800

    Optimized getting_started.rb to perfect Ruby style.
    Change-Id: I043a310e7b7b5c22d1ad4c8bf9b53007227f48d8
    
    Optimized all samples for Ruby Fog.
    
    Change-Id: I043a310e7b7b5c22d1ad4c8bf9b53007227f48d8

--- firstapp/samples/fog/scaling_out.rb
+++ firstapp/samples/fog/scaling_out.rb
@@ -1,98 +1,194 @@
 # step-1
-for instance in conn.servers
-    if ['all-in-one','app-worker-1', 'app-worker-2', 'app-controller'].include?(instance.name)
-        puts 'Destroying Instance: %s' % instance.name
-        conn.delete_server(instance.id)
-    end
+conn.servers.select {|instance| ["all-in-one","app-worker-1", "app-worker-2", "app-controller"].include?(instance.name)}.each do |instance|
+  puts "Destroying Instance: #{instance.name}"
+  instance.destroy
 end
 
-for group in conn.list_security_groups.body["security_groups"]
-    if ['control', 'worker', 'api', 'services'].include?(group["name"])
-        puts 'Deleting security group: %s' % group['name']
-        conn.delete_security_group(group['id'])
-    end
+conn.security_groups.select {|security_group| ["control", "worker", "api", "services"].include?(security_group.name)}.each do |security_group|
+  puts "Deleting security group: #{security_group.name}"
+  security_group.destroy
 end
 
 # step-2
-api_group_response = conn.create_security_group('api', 'for API services only')
-api_group = api_group_response.body["security_group"]
-conn.create_security_group_rule(api_group["id"], 'TCP', '80', '80', '0.0.0.0/0')
-conn.create_security_group_rule(api_group["id"], 'TCP',  '22', '22', '0.0.0.0/0')
+api_group = conn.security_groups.create({
+                                          name:        "api",
+                                          description: "for API services only"
+                                        })
+conn.security_group_rules.create({
+                                   parent_group_id: api_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       80,
+                                   to_port:         80
+                                 })
+conn.security_group_rules.create({
+                                   parent_group_id: api_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       22,
+                                   to_port:         22
+                                 })
 
-worker_group_response = conn.create_security_group('worker', 'for services that run on a worker node')
-worker_group = worker_group_response.body["security_group"]
-conn.create_security_group_rule(worker_group["id"], 'TCP',  '22', '22', '0.0.0.0/0')
+worker_group = conn.security_groups.create({
+                                             name:        "worker",
+                                             description: "for services that run on a worker node"
+                                           })
+conn.security_group_rules.create({
+                                   parent_group_id: worker_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       22,
+                                   to_port:         22
+                                 })
 
-services_group_response = conn.create_security_group('services', 'for DB and AMQP services only')
-services_group = services_group_response.body["security_group"]
-conn.create_security_group_rule(services_group["id"], 'TCP', '80', '80', '0.0.0.0/0')
-conn.create_security_group_rule(services_group["id"], 'TCP',  '22', '22', '0.0.0.0/0')
-conn.create_security_group_rule(services_group["id"], 'TCP',  '3306', '3306', '0.0.0.0/0', api_group["id"])
-conn.create_security_group_rule(services_group["id"], 'TCP',  '5672', '5672', '0.0.0.0/0', worker_group["id"])
-conn.create_security_group_rule(services_group["id"], 'TCP',  '5672', '5672', '0.0.0.0/0', api_group["id"])
+controller_group = conn.security_groups.create({
+                                                 name:        "control",
+                                                 description: "for services that run on a control node"
+                                               })
+conn.security_group_rules.create({
+                                   parent_group_id: controller_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       22,
+                                   to_port:         22
+                                 })
+conn.security_group_rules.create({
+                                   parent_group_id: controller_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       80,
+                                   to_port:         80
+                                 })
+conn.security_group_rules.create({
+                                   parent_group_id: controller_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       5672,
+                                   to_port:         5672,
+                                   group:           worker_group.id
+                                 })
+
+services_group = conn.security_groups.create({
+                                               name:        "services",
+                                               description: "for DB and AMQP services only"
+                                             })
+conn.security_group_rules.create({
+                                   parent_group_id: services_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       22,
+                                   to_port:         22
+                                 })
+conn.security_group_rules.create({
+                                   parent_group_id: services_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       3306,
+                                   to_port:         3306,
+                                   group:           api_group.id
+                                 })
+conn.security_group_rules.create({
+                                   parent_group_id: services_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       5672,
+                                   to_port:         5672,
+                                   group:           worker_group.id
+                                 })
+conn.security_group_rules.create({
+                                   parent_group_id: services_group.id,
+                                   ip_protocol:     "tcp",
+                                   from_port:       5672,
+                                   to_port:         5672,
+                                   group:           api_group.id
+                                 })
 
 # step-3
-def get_floating_ip (conn)
-    '''A helper function to re-use available Floating IPs'''
-    unused_floating_ip = nil
-    for floating_ip in conn.list_all_addresses.body["floating_ips"]
-        if not floating_ip["instance_id"]
-            unused_floating_ip = floating_ip
-            break
-        end
-    end
-    if not unused_floating_ip
-        pool = conn.list_address_pools.body["floating_ip_pools"][0]
-        puts 'Allocating new Floating IP from pool: {}'. pool
-        unused_floating_ip = conn.allocate_address(pool)
-    end
-    return unused_floating_ip
+def get_floating_ip_address(conn)
+  unless unused_floating_ip_address = conn.addresses.find {|address| address.instance_id.nil?}
+    pool_name = conn.addresses.get_address_pools[0]["name"]
+    puts "Allocating new Floating IP from pool: #{pool_name}"
+    unused_floating_ip_address = conn.addresses.create({
+                                                         pool: pool_name
+                                                       })
+  end
+
+  unused_floating_ip_address
 end
 
 # step-4
-userdata = '''#!/usr/bin/env bash
+user_data = <<END
+#!/usr/bin/env bash
 curl -L -s http://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
-    -i database -i messaging
-'''
+-i database -i messaging
+END
 
-instance_services= conn.servers.create(:name => 'app-services', :flavor_ref => flavor.id, :image_ref => image.id, :key_name => 'demokey', :user_data => userdata, :security_groups => [services_group["name"]])
-
-instance_services.wait_for { ready? }
-services_ip = instance_services.accessIPv4
-
+instance_services = conn.servers.create({
+                                          name:            "app-services",
+                                          image_ref:       image.id,
+                                          flavor_ref:      flavor.id,
+                                          key_name:        "demokey",
+                                          user_data:       user_data,
+                                          security_groups: services_group
+                                        })
+Fog.wait_for {instance_services.ready?}
+services_ip_address = instance_services.private_ip_address
 
 # step-5
-userdata = "#!/usr/bin/env bash
+user_data = <<END
+#!/usr/bin/env bash
 curl -L -s http://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
-    -i faafo -r api -m 'amqp://guest:guest@#{services_ip}:5672/' \
-    -d 'mysql://faafo:password@#{services_ip}:3306/faafo'
-"
+-i faafo -r api -m "amqp://guest:guest@#{services_ip_address}:5672/" -d "mysql+pymysql://faafo:password@#{services_ip_address}:3306/faafo"
+END
 
-instance_api_1 = conn.servers.create(:name => 'app-api-1', :flavor_ref => flavor.id, :image_ref => image.id, :key_name => 'demokey', :user_data => userdata, :security_groups => [api_group["name"]])
-instance_api_2 = conn.servers.create(:name => 'app-api-2', :flavor_ref => flavor.id, :image_ref => image.id, :key_name => 'demokey', :user_data => userdata, :security_groups => [api_group["name"]])
+instance_api_1 = conn.servers.create({
+                                       name:            "app-api-1",
+                                       image_ref:       image.id,
+                                       flavor_ref:      flavor.id,
+                                       key_name:        "demokey",
+                                       user_data:       user_data,
+                                       security_groups: api_group
+                                     })
+instance_api_2 = conn.servers.create({
+                                       name:            "app-api-2",
+                                       image_ref:       image.id,
+                                       flavor_ref:      flavor.id,
+                                       key_name:        "demokey",
+                                       user_data:       user_data,
+                                       security_groups: api_group
+                                     })
+Fog.wait_for {instance_api_1.ready?}
+api_1_ip_address = instance_api_1.private_ip_address
+Fog.wait_for {instance_api_2.ready?}
+api_2_ip_address = instance_api_2.private_ip_address
 
-
-instance_api_1.wait_for { ready? }
-api_1_ip = instance_api_1.accessIPv4
-instance_api_2.wait_for { ready? }
-api_2_ip = instance_api_2.accessIPv4
-
-
-
-for instance in [instance_api_1,  instance_api_2]
-    floating_ip = get_floating_ip(conn)
-    instance.associate_address(floating_ip["ip"])
-    puts "allocated #{floating_ip["ip"]} to #{instance.name}"
+[instance_api_1, instance_api_2].each do |instance|
+  floating_ip_address = get_floating_ip_address(conn)
+  floating_ip_address.server = instance
+  puts "allocated #{floating_ip_address.ip} to #{instance.name}"
 end
 
 # step-6
-userdata = "#!/usr/bin/env bash
+user_data = <<END
+#!/usr/bin/env bash
 curl -L -s http://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
-    -i faafo -r worker -e 'http://#{api_1_ip}' -m 'amqp://guest:guest@#{services_ip}:5672/'
-"
+-i faafo -r worker -e "http://#{api_1_ip_address}" -m "amqp://guest:guest@#{services_ip_address}:5672/"
+END
 
-instance_worker_1 = conn.servers.create(:name => 'app-worker-1', :flavor_ref => flavor.id, :image_ref => image.id, :key_name => 'demokey', :user_data => userdata, :security_groups => [worker_group["name"]])
-instance_worker_2 = conn.servers.create(:name => 'app-worker-2', :flavor_ref => flavor.id, :image_ref => image.id, :key_name => 'demokey', :user_data => userdata, :security_groups => [worker_group["name"]])
-instance_worker_3 = conn.servers.create(:name => 'app-worker-3', :flavor_ref => flavor.id, :image_ref => image.id, :key_name => 'demokey', :user_data => userdata, :security_groups => [worker_group["name"]])
+instance_worker_1 = conn.servers.create({
+                                          name:            "app-worker-1",
+                                          image_ref:       image.id,
+                                          flavor_ref:      flavor.id,
+                                          key_name:        "demokey",
+                                          user_data:       user_data,
+                                          security_groups: worker_group
+                                        })
+instance_worker_2 = conn.servers.create({
+                                          name:            "app-worker-2",
+                                          image_ref:       image.id,
+                                          flavor_ref:      flavor.id,
+                                          key_name:        "demokey",
+                                          user_data:       user_data,
+                                          security_groups: worker_group
+                                        })
+instance_worker_3 = conn.servers.create({
+                                          name:            "app-worker-3",
+                                          image_ref:       image.id,
+                                          flavor_ref:      flavor.id,
+                                          key_name:        "demokey",
+                                          user_data:       user_data,
+                                          security_groups: worker_group
+                                        })
 
 # step-7
